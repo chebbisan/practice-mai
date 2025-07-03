519a569d1f92bffb339454e396101d372b18de28
chebbisan <fngsupersteal@gmail.com>
2025-07-03 14:48:53 +0300

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+1. Бибилиотека на c++ (dll)
+    Sum(double, double)
+    SumComplex()
+2. SumComplexArray() -> sum_array
+3. Sum(func* )
+
+4. Диаграмма решетки
+F(theta, phi) = K * Sum(A_n * exp(-j ))
++ еще тестовая версия в MathCad
+Сроки: до след чт, первые 3 пункта. 
+
+GitHub: Virtual Machine работать тут
+*/
+
